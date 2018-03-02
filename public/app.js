8a4834a1969dec7b48a607073281b5b2020fa1ab
Allie <allie@codefellows.com>
2018-03-01 16:55:50 -0800

    adding demo after class

--- public/app.js
+++ public/app.js
@@ -1,14 +1,20 @@
 'use strict';
 
-$.ajax({
-  url: 'https://api.github.com/user/repos?type=owner',
-  method: 'GET',
-  headers: {
-    Authorization: 'token paste-your-token-here'
-  }
-})
+// $.ajax({
+//   url: 'https://api.github.com/user/repos?type=owner',
+//   method: 'GET',
+//   headers: {
+//     Authorization: `token ${githubToken}`
+//   }
+// })
+
+$.get('/github/user/repos')
+  .then(data => {
+    console.log(data);
+    return data;
+  })
   .then(
-    data => data.forEach(repo => 
+    data => JSON.parse(data).forEach(repo => 
       $('#results').append(`<h3>${repo.name}</h3><p>${repo.description}</p><hr>`)),
       err => console.error(err.status, err.statusText, 'is the reason this is broken')
 )
--- server.js
+++ server.js
@@ -4,12 +4,22 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT;
 const cors = require('cors');
+const superagent = require('superagent');
 
 app.use(express.static('./public'));
 app.use(cors());
 
 app.get('/', (request, response) => {
   response.sendFile('index.html');
+})
+
+app.get('/github/*', (request, response) => {
+  console.log('Routing for:', request.params[0]);
+  const url = `https://api.github.com/${request.params[0]}`;
+  superagent(url)
+    .set(`Authorization`, `token ${process.env.TOKEN}`)
+    .then(repos => response.send(repos.text))
+    .catch(console.error)
 })
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
